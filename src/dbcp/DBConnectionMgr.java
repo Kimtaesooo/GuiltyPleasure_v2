a1ade8dec8c3cff0d4cc582089d5061e7890372a
Taesoo <kst6612@naver.com>
2017-07-01 18:25:21 +0900

    와우!

--- src/dbcp/DBConnectionMgr.java
+++ src/dbcp/DBConnectionMgr.java
@@ -40,8 +40,8 @@ public class DBConnectionMgr {
     private Vector<ConnectionObject> connections = new Vector<ConnectionObject>(10);
     
     private String _driver = "oracle.jdbc.driver.OracleDriver",
-    //_url = "jdbc:oracle:thin:@70.12.110.106:1521:orcl",
-    _url = "jdbc:oracle:thin:@localhost:1521:orcl",
+    //_url = "jdbc:oracle:thin:@localhost:1521:orcl",
+    _url = "jdbc:oracle:thin:@70.12.110.106:1521:orcl",
     _user = "GP",
     _password = "1111";
 
